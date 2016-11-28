1cc22161efe8a9b29e04d97e4a56c7b6334ea06e
codeEmitter <greg.johnson@versionone.com>
2016-11-28 13:29:10 -0500

    begin test driving a static method to test the db connection

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+export default class MssqlSnapshot {
+    static testConnect(config){
+        throw new Error('No configuration information supplied.')
+    }
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,14 @@
 import 'babel-polyfill';
 import { expect, before } from 'chai';
 
+import MssqlSnapshot from '../src/index';
+
+describe("when testing the database connection", () => {
+    it("config info must be supplied", function() {
+        expect(MssqlSnapshot.testConnect).to.throw(/No configuration information supplied./);
+    });
+});
+
 describe("when connecting to mssql database via a connection string", () => {
     beforeEach(function(){
         let current = 'a,b,c'.split(',');
