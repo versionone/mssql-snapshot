1826931aaab75a171687d9bce783a299580f9394
codeEmitter <greg.johnson@versionone.com>
2016-12-14 10:40:23 -0500

    test fail case for connection testing

--- src/MssqlSnapshot.js
+++ src/MssqlSnapshot.js
@@ -3,7 +3,14 @@ import sql from 'mssql';
 
 export default class MssqlSnapshot {
     static testConnection(config) {
-        return sql.connect(config).then((result) => new sql.Request().query("SELECT 'Succeeded' as CONNECTION"));
+        return new Promise((resolve, reject)=> {
+            try{
+                resolve(sql.connect(config).then((result) => new sql.Request().query("SELECT 'Succeeded' as CONNECTION")));
+            }
+            catch(error) {
+                reject(error);
+            }
+        });
     }
 }
 
--- test/MssqlSnapshot.testConnection.spec.js
+++ test/MssqlSnapshot.testConnection.spec.js
@@ -8,7 +8,10 @@ chai.should();
 import MssqlSnapshot from '../src/MssqlSnapshot';
 
 describe("when testing the database connection", () => {
-    it("succeeds with the correct connection string", (done) => {
-        return MssqlSnapshot.testConnection("mssql://pub:pub@localhost/V1Demo").should.eventually.become([{CONNECTION: 'Succeeded'}]).notify(done);
+    it("succeeds with the correct connection string", () => {
+        return MssqlSnapshot.testConnection("mssql://pub:pub@localhost/V1Demo").should.eventually.become([{CONNECTION: 'Succeeded'}]);
+    });
+    it("fails with an appropriate exception message when config information is missing", () => {
+       return MssqlSnapshot.testConnection().should.eventually.be.rejectedWith(Error);
     });
 });
