51025eb09aca253b78586810460b375710632e7f
codeEmitter <greg.johnson@versionone.com>
2016-12-23 00:16:59 -0500

    describe use case for fakedb

--- test/databaseConfig.js
+++ test/databaseConfig.js
@@ -1,7 +1,7 @@
 export default function(fakeDb) {
     return {
         isUnitTest: true, //value should equal false to run integration tests using the connection inforamtion below
-        fakeDb: fakeDb,
+        fakeDb: fakeDb, //unit tests should expect a fake database object with mocked props to pass the tests and match the api
         user: 'user',
         password: 'pass',
         server: 'localhost',
