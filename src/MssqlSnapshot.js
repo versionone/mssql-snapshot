a4c18c255b01c5f9ade5fe7d3db756c4a7ba70f5
codeEmitter <greg.johnson@versionone.com>
2016-12-28 14:11:15 -0500

    cleanup

--- src/MssqlSnapshot.js
+++ src/MssqlSnapshot.js
@@ -1,12 +1,11 @@
 export default class MssqlSnapshot {
-    constructor(db) {
-        this.db = db.retrieve();
-        this.config = db.config;
+    constructor(database) {
+        this.database = database.retrieve();
+        this.config = database.config;
     }
     testConnection() {
         return new Promise((resolve, reject) => {
-            console.log(this.config);
-            this.db.connect(this.config, (err) => {
+            this.database.connect(this.config, (err) => {
                 if (err) {
                     reject(new Error(""));
                 } else {
