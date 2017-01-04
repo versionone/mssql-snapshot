de9b4f715ad2660393df868c3b1ae0249a1ef639
codeEmitter <greg.johnson@versionone.com>
2017-01-04 09:59:44 -0500

    Break up test files appropriately

---
+++ test/database.constructor.spec.js
@@ -0,0 +1,16 @@
+import chai from 'chai';
+import sql from 'seriate';
+
+chai.should();
+
+import Database from '../src/Database';
+
+describe("when retrieving a database object to perform queries", function() {
+    it("it doesnt throw when configuration is supplied", () => {
+        new Database({}).should.not.throw;
+    });
+    it("it throws an error when no configuration object is supplied", () => {
+        const fn = () => new Database();
+        fn.should.throw("No configuration supplied to orchestrate the connection interface.");
+    });
+});
--- test/database.retrieve.spec.js
+++ test/database.retrieve.spec.js
@@ -9,8 +9,4 @@ describe("when retrieving a database object to perform queries", function() {
     it("it returns a seriate npm module when configuration is supplied", () => {
         new Database({}).retrieve().should.eql(sql);
     });
-    it("it throws an error when no configuration object is supplied", () => {
-        const fn = () => new Database();
-        fn.should.throw("No configuration supplied to orchestrate the connection interface.");
-    });
 });
