9d8750ce25f6fcc3ac88706cdf3e2d8144069964
codeEmitter <greg.johnson@versionone.com>
2017-01-05 16:01:54 -0500

    No results are returned when the config is invalid.  Why no errro?

--- test/MssqlSnapshot.listSnapshots.spec.js
+++ test/MssqlSnapshot.listSnapshots.spec.js
@@ -12,11 +12,28 @@ describe("when retrieving a list of snapshots", function() {
         dbConfig = databaseConfig();
         target = new MssqlSnapshot(dbConfig);
     });
-    it("it returns one result", (done) => {
+    it("it returns one result when configuration is valid", (done) => {
         target.listSnapshots().then(
             (result) => {
                 result.length.should.be.greaterThan(0);
                 done();
+            });
+    });
+});
+
+describe("when retrieving a list of snapshots", function() {
+    let target = null;
+    beforeEach(function() {
+        target = new MssqlSnapshot({});
+    });
+    it("it returns no results when the configuration is invalid", (done) => {
+        target.listSnapshots().then(
+            (result) => {
+                result.length.should.eql(0);
+                done();
+            },
+            (err) => {
+                done(err);  //why does this not throw an error if the config is bad?
             });
     });
 });
