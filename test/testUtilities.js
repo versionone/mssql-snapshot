25da9dbc08803319d46e1109aeca138e4f2bf3a5
codeEmitter <greg.johnson@versionone.com>
2017-03-07 16:16:09 -0500

    DRY up paramater declarations

--- test/testUtilities.js
+++ test/testUtilities.js
@@ -15,14 +15,8 @@ export function deleteSnapshot(snapshotName) {
     return sql.execute(config.name, {
         query: sql.fromFile('../src/queries/deleteSnapshot.sql'),
         params: {
-            snapshotName: {
-                val: snapshotName,
-                type: sql.VARCHAR(100)
-            },
-            query: {
-                val: '',
-                type: sql.VARCHAR(300)
-            }
+            snapshotName: Parameters.snapshotName(snapshotName),
+            query: Parameters.query
         }
     });
 }
@@ -33,22 +27,10 @@ export function createSnapshot(snapshotName) {
     return sql.execute(config.name, {
         query: sql.fromFile('../src/queries/createSnapshot.sql'),
         params: {
-            query: {
-                val: '',
-                type: sql.VARCHAR(300)
-            },
-            sourceDbName: {
-                val: config.database,
-                type: sql.VARCHAR(50)
-            },
-            snapshotName: {
-                val: snapshotName,
-                type: sql.VARCHAR(50)
-            },
-            snapshotPath: {
-                val: config.snapshotStoragePath + snapshotName,
-                type: sql.VARCHAR(200)
-            }
+            query: Parameters.query,
+            sourceDbName: Parameters.sourceDbName,
+            snapshotName: Parameters.snapshotName(snapshotName),
+            snapshotPath: Parameters.snapshotPath(snapshotName)
         }
     });
 }
