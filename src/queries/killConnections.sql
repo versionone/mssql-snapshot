5f2f87fab3acf081d51e3f5193440cf9e6622331
codeEmitter <greg.johnson@versionone.com>
2017-03-07 16:18:10 -0500

    kill all connections to the db before attempting to accurately test connection counts

---
+++ src/queries/killConnections.sql
@@ -0,0 +1,4 @@
+SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'
+FROM sys.dm_exec_sessions
+WHERE database_id  = db_id(@sourceDbName)
+EXEC(@kill);
--- test/testUtilities.js
+++ test/testUtilities.js
@@ -1,11 +1,22 @@
 import sql from 'seriate';
 import databaseConfig from '../src/databaseConfig'
+import * as Parameters from '../src/Parameters';
 
 export function createConnection() {
     const config = databaseConfig();
-    sql.addConnection(config);
     return sql.execute(config.name, {
        query: `SELECT TABLE_NAME FROM [${config.database}].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'`
+    });
+}
+
+export function killConnections() {
+    const config = databaseConfig();
+    return sql.execute(config.name, {
+        query: sql.fromFile('../src/queries/killConnections.sql'),
+        params: {
+            sourceDbName: Parameters.sourceDbName,
+            kill: sql.VARCHAR(8000),
+        }
     });
 }
 
