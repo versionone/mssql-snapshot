7e4bade5febd3813c77ad7d77f513e02d8dc4479
codeEmitter <greg.johnson@versionone.com>
2017-04-14 09:41:22 -0400

    Trim output from sysprocesses since they're not variable length...variables.  yea.

--- src/queries/connections.sql
+++ src/queries/connections.sql
@@ -1,14 +1,14 @@
 SELECT  spid SPID,
-        sp.[status] Status,
-        loginame [Login],
-        hostname HostName,
-        blocked BlockedBy,
-        sd.name DatabaseName,
-        cmd Command,
+        RTRIM(sp.[status]) Status,
+        RTRIM(loginame) [Login],
+        RTRIM(hostname) HostName,
+        RTRIM(blocked) BlockedBy,
+        RTRIM(sd.name) DatabaseName,
+        RTRIM(cmd) Command,
         cpu CPUTime,
         physical_io DiskIO,
-        last_batch LastBatch,
-        [program_name] ProgramName
+        RTRIM(last_batch) LastBatch,
+        RTRIM([program_name]) ProgramName
 FROM master.dbo.sysprocesses sp
 	JOIN master.dbo.sysdatabases sd ON sp.dbid = sd.dbid
 WHERE
