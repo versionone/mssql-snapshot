13b121d03a1731a1343949959e7682e09b12271f
codeEmitter <greg.johnson@versionone.com>
2017-05-02 09:17:05 -0400

    refactor in order to avoid hard coded paths in tests

--- test/MssqlSnapshot.getSnapshotStoragePath.spec.js
+++ test/MssqlSnapshot.getSnapshotStoragePath.spec.js
@@ -2,26 +2,28 @@ import path from 'path';
 
 import MssqlSnapshot from '../src/MssqlSnapshot';
 import databaseConfig from '../src/databaseConfig';
+import {getPhysicalPath} from './testUtilities';
 
 describe('when retrieving the storage path for a snapshot', function() {
-	let targetWithSnapshotStoragePath, configWithSnapshotStoragePath, defaultTarget = null;
+	let targetWithSnapshotStoragePath, configWithSnapshotStoragePath, defaultTarget, expectedUnconfiguredPath, expectedConfiguredPath = null;
 	const snapshotName = 'mssql-snapshot-testdb-get-path';
-	const expectedPath = path.join('C:\\Program Files\\Microsoft SQL Server\\MSSQL12.MSSQLSERVER\\MSSQL\\DATA', snapshotName);
 
 	beforeEach(() => {
 		configWithSnapshotStoragePath = Object.assign(databaseConfig(), {snapshotStoragePath: 'c:\\snapshots'});
+		expectedConfiguredPath = path.join(configWithSnapshotStoragePath.snapshotStoragePath, snapshotName);
 		targetWithSnapshotStoragePath = new MssqlSnapshot(configWithSnapshotStoragePath);
 		defaultTarget = new MssqlSnapshot(databaseConfig());
+		return getPhysicalPath(databaseConfig().database, snapshotName).then((result) => expectedUnconfiguredPath = result);
 	});
 
 	it('it returns the path defined in config if one is present', function() {
 		return targetWithSnapshotStoragePath.getSnapshotStoragePath(snapshotName, configWithSnapshotStoragePath.snapshotStoragePath)
-			.should.eventually.equal('c:\\snapshots\\mssql-snapshot-testdb-get-path');
+			.should.eventually.equal(expectedConfiguredPath);
 	});
 
 	it('it returns the path that the source db is stored if no storage path is defined', function() {
 		return defaultTarget.getSnapshotStoragePath(snapshotName)
-			.should.eventually.eql(expectedPath);
+			.should.eventually.eql(expectedUnconfiguredPath);
 	});
 
 });
